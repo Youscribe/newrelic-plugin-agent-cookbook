4f5e45ebc7043562dd468f35d75ae520178e96e7
Yann ROBIN <me.show@gmail.com>
2013-07-02 20:21:20 +0200

    made some change according to recommendation

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,11 @@
-default[:newrelic][:user]      = "newrelic"
-default[:newrelic][:group]     = "adm"
-default[:newrelic][:home]      = "/usr/share/#{newrelic.user}"
-default[:newrelic][:run_dir]   = "/var/run/#{newrelic.user}"
-default[:newrelic][:log_dir]   = "/var/log/#{newrelic.user}"
+default["newrelic"]["user"]      = "newrelic"
+default["newrelic"]["group"]     = "adm"
+default["newrelic"]["home"]      = "/usr/share/newrelic"
+default["newrelic"]["run_dir"]   = "/var/run/newrelic"
+default["newrelic"]["log_dir"]   = "/var/log/newrelic"
+default["newrelic"]["config_dir"]   = "/etc/newrelic"
 
-default[:newrelic][:poll_interval] = 60
-default[:newrelic][:pid_file] = "#{newrelic.run_dir}/newrelic_plugin_agent.pid"
-default[:newrelic][:log_file] = "#{newrelic.log_dir}/newrelic_plugin_agent.log"
+default["newrelic"]["poll_interval"] = 60
+default["newrelic"]["pid_file"] = "#{newrelic['run_dir']}/newrelic_plugin_agent.pid"
+default["newrelic"]["log_file"] = "#{newrelic['log_dir']}/newrelic_plugin_agent.log"
+default["newrelic"]["config_file"] = "#{newrelic['config_dir']}/newrelic_plugin_agent.cfg"
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,39 +9,39 @@
 
 include_recipe "python::pip"
 
-user node.newrelic.user do
-  gid   node.newrelic.group
+user node["newrelic"]["user"] do
+  gid   node["newrelic"]["group"]
   shell '/bin/false'
-  home  node.newrelic.home
+  home  node["newrelic"]["home"]
 end
 
-directory node.newrelic.run_dir do
-  owner node.newrelic.user
-  group node.newrelic.group
+directory node["newrelic"]["run_dir"] do
+  owner node["newrelic"]["user"]
+  group node["newrelic"]["group"]
   mode  '755'
 end
 
-directory node.newrelic.log_dir do
-  owner node.newrelic.user
-  group node.newrelic.group
+directory node["newrelic"]["log_dir"] do
+  owner node["newrelic"]["user"]
+  group node["newrelic"]["group"]
   mode  '700'
 end
 
 python_pip "newrelic-plugin-agent"
 
-template "/etc/newrelic/newrelic_plugin_agent.cfg" do
+template node["newrelic"]["config_file"] do
 	source "newrelic_plugin_agent.cfg.erb"
 	mode "0600"
 	variables (
-		:hostname => node[:hostname],
-		:license_key => node[:newrelic][:license_key],
-		:poll_interval => node[:newrelic][:poll_interval],
-		:user => node[:newrelic][:user],
-		:pid_file => node[:newrelic][:pid_file],
-		:log_file => node[:newrelic][:log_file],
-		:mongodb => node[:newrelic][:mongodb],
-		:nginx => node[:newrelic][:nginx],
-		:postgresql => node[:newrelic][:postgresql],
-		:redis => node[:newrelic][:redis]
+		:hostname => node["hostname"],
+		:license_key => node["newrelic"]["license_key"],
+		:poll_interval => node["newrelic"]["poll_interval"],
+		:user => node["newrelic"]["user"],
+		:pid_file => node["newrelic"]["pid_file"],
+		:log_file => node["newrelic"]["log_file"],
+		:mongodb => node["newrelic"]["mongodb"],
+		:nginx => node["newrelic"]["nginx"],
+		:postgresql => node["newrelic"]["postgresql"],
+		:redis => node["newrelic"]["redis"]
 	)
 end
